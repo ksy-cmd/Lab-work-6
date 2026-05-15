86ad6674b79d617460fd4ac1b5237e959bdf1f3e
ksy-cmd <krastikrabs200@gmail.com>
2026-05-15 18:20:42 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
+  base: './',  // ← ДОДАТИ ЦЕ (відносний шлях для GitHub Pages)
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url))
     },
   },
 })
